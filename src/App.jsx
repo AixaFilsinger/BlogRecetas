ed980da58603112ee127a7e912f3ea3f08623995
Aixa Filsinger <macafil99@gmail.com>
2023-06-21 06:10:20 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/common/Footer";
 import DetalleRecetas from "./components/views/DetalleRecetas";
 import Login from "./components/views/Login";
-
-
 import Inicio from "./components/views/Inicio";
 import Error404 from "./components/views/Error404";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import { useState } from "react";
 import RutasProtegidas from "./components/routes/RutasProtegidas";
 import RutasAdmin from "./components/routes/RutasAdmin";
 
 function App() {
-  const usuarioLocal = JSON.parse(localStorage.getItem("usuario")) || {};
+  const usuarioLocal = JSON.parse(localStorage.getItem("usuario")) || null;
 
   const [usuarioLogueado, setUsuarioLogueado] = useState(usuarioLocal);
 
